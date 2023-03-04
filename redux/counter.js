6f1fbf404e22544621404b0a23e9320bb11de03d
Moshiul <gazimoshiul@gmail.com>
2023-03-04 21:30:13 +0600

    redux-store-counter

---
+++ redux/counter.js
@@ -0,0 +1,65 @@
+// state - count:0
+// action - increment, decrement, reset
+// reducer
+// store
+
+const {createStore} = require("redux");
+
+// constant
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECREMENT'
+const RESET = 'RESET'
+
+const initialState ={
+    count:0,
+    // amount:0
+}
+
+const incrementAction=()=>{
+    return{
+        type:INCREMENT
+    }
+}
+const decrementAction=()=>{
+    return{
+        type:DECREMENT
+    }
+}
+const resetCounterAction=()=>{
+    return{
+        type:RESET
+    }
+}
+
+// creating reducer
+const counterReducer =(state=initialState, action)=>{
+    switch (action.type) {
+        case INCREMENT:
+            return {
+                ...state,
+                count: state.count+1
+            }
+        case DECREMENT:
+            return {
+                ...state,
+                count: state.count-1
+            }  
+        case RESET:
+            return {
+                ...state,
+                count: 0
+            }  
+    
+        default:
+            state;
+    }
+}
+
+// store
+const store = createStore(counterReducer);
+
+store.subscribe(()=>{
+    console.log(store.getState())
+});
+
+store.dispatch(incrementAction())
--- redux/index.js
+++ redux/index.js
@@ -1,45 +1,83 @@
+const {createStore} = require("redux");
+
 // defining constants
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const ADD_USER = "ADD_USER";
 
-// state
+// 01. state
 const initialCounterState = {
-    count: 0,
-}
+  count: 0,
+};
 
 const initialUsersState = {
-    users: [
-        { name: 'Moshiul' }
-    ]
-}
+  users: [{ name: "Moshiul" }],
+};
 
-// action - OBJECT - TYPE (অবশ্যই থাকতে হবে), PAYLOAD (কোন ডাটা পাঠানোর জন্য ব্যবহার করাহয়)
+// 02. action - OBJECT - TYPE (অবশ্যই থাকতে হবে), PAYLOAD (কোন ডাটা পাঠানোর জন্য ব্যবহার করা হয়)
 
 // INCREMENT COUNTER
 // DECREMENT COUNTER
 
 const incrementCounter = () => {
-    return {
-        type: INCREMENT,
-    }
-}
+  return {
+    type: INCREMENT,
+  };
+};
 const decrementCounter = () => {
-    return {
-        type: DECREMENT,
-    }
-}
+  return {
+    type: DECREMENT,
+  };
+};
 const addUser = () => {
-    return {
-        type: ADD_USER,
-        payload:{
-            name:"Kazi Madu"
-        }
-    }
-}
+  return {
+    type: ADD_USER,
+    payload: {
+      name: "Kazi Madu",
+    },
+  };
+};
+
+// 03. create reducer for counter
+
+// const counterReducer = (state={count:0}, action)=>{
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      // increment related staff goes here
+      return {
+        ...state, // if state contain multiple object
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      // decrement related staff goes here
+      return {
+        ...state, // if state contain multiple object
+        count: state.count - 1,
+      };
+
+    default:
+      state;
+  }
+};
+
+
+// 04. store - getState(), dispatch(), subscribe()
+
+// create store
+const store = createStore(counterReducer);
+store.subscribe(()=>{
+    console.log(store.getState());
+})
+
+// dispatch action
+store.dispatch(incrementCounter())
+store.dispatch(incrementCounter())
+store.dispatch(decrementCounter())
 
 
 // 1. state
 // 2. dispatch action
 // 3. reducer (work based on action type)
-// 4. 
+// 4. store
